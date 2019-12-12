9339b252be03a55306fa92ce978809c19f35b389
vikakrylova <vika.krylova.2011@gmail.com>
2019-12-12 21:28:26 +0300

    1

---
+++ hello/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <h1>Hello world!</h1>
+  );
+}
+
+export default App;
